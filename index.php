40fc6e0e80438ba62f5ab450a14f293ab80148fc
tamazs <majortamas313@gmail.com>
2021-09-16 14:47:36 +0200

    landing added, skeleton done, responsive tbd

--- index.php
+++ index.php
@@ -1,12 +1,68 @@
-<div class="content">
 <?php
     get_header();
-        if (have_posts()) :
-            while (have_posts()) : the_post(); ?>
-                <h1><?php the_title(); ?></h1>
-                <p><?php the_content() ?></p>
-            <?php endwhile;
-        endif;
+?>
+<div class="landing">
+    <img src="<?php echo get_template_directory_uri() ?>/images/landingc.png" alt="image" class="landingimg">
+    <div class="toptext"><?php bloginfo('name')?></div>
+    <div class="bottomtext"><?php bloginfo('description')?></div>
+</div>
+<div class="content">
+    <div class="container">
+        <section class="p1">
+            <h1>Poland</h1>
+        </section>
+        <section class="p2">
+            <h1>Poland</h1>
+        </section>
+        <section class="p3">
+            <h1>Poland</h1>
+        </section>
+    </div>
+    <div class="container">
+        <section class="t1">
+            <h1>Texas</h1>
+        </section>
+        <section class="t2">
+            <h1>Texas</h1>
+        </section>
+        <section class="t3">
+            <h1>Texas</h1>
+        </section>
+    </div>
+    <div class="container">
+        <section class="a1">
+            <h1>Afghanistan</h1>
+        </section>
+        <section class="a2">
+            <h1>Afghanistan</h1>
+        </section>
+        <section class="a3">
+            <h1>Afghanistan</h1>
+        </section>
+    </div>
+    <div class="container">
+        <section class="j1">
+            <h1>Japan</h1>
+        </section>
+        <section class="j2">
+            <h1>Japan</h1>
+        </section>
+        <section class="j3">
+            <h1>Japan</h1>
+        </section>
+    </div>
+    <div class="container">
+        <section class="g1">
+            <h1>Global</h1>
+        </section>
+        <section class="g2">
+            <h1>Global</h1>
+        </section>
+        <section class="g3">
+            <h1>Global</h1>
+        </section>
+    </div>
+</div>
+<?php
     get_footer();
 ?>
-</div>
